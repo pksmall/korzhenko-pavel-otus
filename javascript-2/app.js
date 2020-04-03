51db99a5a2c781fc449d17fe291c135383ca30e8
Pavel Korzhenko <pksmall@live.ru>
2020-04-03 18:33:42 -0400

    add support idea

--- javascript-2/app.js
+++ javascript-2/app.js
@@ -1,29 +1,29 @@
 'use strict';
 
 var fn1 = () => {
-    console.log('fn1')
+    console.log('fn1');
     return Promise.resolve(1)
-}
+};
 
 var fn2 = () => new Promise(resolve => {
-    console.log('fn2')
+    console.log('fn2');
     setTimeout(() => resolve(2), 1000)
-})
+});
 
 var fn3 = () => new Promise(resolve => {
-    console.log('fn3')
+    console.log('fn3');
     setTimeout(() => resolve(3), 1000)
-})
+});
 
 var fn4 = () => new Promise(resolve => {
-    console.log('fn4')
+    console.log('fn4');
     setTimeout(() => resolve(4), 1000)
-})
+});
 
 var reduce = (m, v) => {
-    console.log('reduce')
+    console.log('reduce');
     return m * v;
-}
+};
 
 async function testArr(aFunc, rduce, initVal) {
     return aFunc.reduce((p, fn) => p.then(fn).then(v => rduce(v, initVal)), Promise.resolve());
@@ -39,10 +39,10 @@ function promiseReduce(asyncFunctions, reduce, initialValue) {
 promiseReduce(
     [fn1, fn2],
     function (memo, value) {
-        console.log('reduce')
+        console.log('reduce');
         return memo * value
     },
     1
 ).then(console.log);
 
-//setTimeout(() => { console.log("It's The End!"); }, 10000);
+setTimeout(() => { console.log("It's The End!"); }, 10000);

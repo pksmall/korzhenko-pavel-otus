4ac4f20a17c75beb422fecf6955427a59f091748
Pavel Korzhenko <pksmall@live.ru>
2020-04-03 18:27:30 -0400

    add support vs code

--- javascript-1/app.js
+++ javascript-1/app.js
@@ -18,4 +18,5 @@ const infiniteCurry = fn => {
 const iSum = infiniteCurry((x, y) => x + y);
 console.log(iSum(1)(3)(4)(2)(5)(8)(11)(55) ());
 
+
 setTimeout(() => { console.log("It's The End!"); }, 10000);

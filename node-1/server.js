6ccf9a5029596696fdf3336de369a41585d3d2cb
Pavel Korzhenko <pksmall@live.ru>
2020-04-15 14:07:24 +0300

    fix server timeout

--- node-1/server.js
+++ node-1/server.js
@@ -7,7 +7,7 @@ let server = http.createServer(function (req, res) {
         console.log("got request!");
         res.writeHead(200, {'Content-Type': 'text/plain'});
         res.end('Hello World\n');
-    }, 1000);
+    }, 100);
 }).listen(3002);
 
 console.log('Server running at http://127.0.0.1:3002/');

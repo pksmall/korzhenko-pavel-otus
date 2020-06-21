616efb40a797ba046080c457afd0f5c48142abe2
Pavel Korzhenko <pksmall@live.ru>
2020-06-21 20:46:38 +0300

    add test

--- node-4/test/index.test.js
+++ node-4/test/index.test.js
@@ -1,15 +1,21 @@
 const app = require('../src/app');
-const {startDatabase} = require('../src/database/mongo');
-const {insertAd} = require('../src/ads/ads');
+const supertest = require('supertest')
+const request = supertest(app)
 
-// A simple example test
-describe('#Test RestApi function', () => {
-    it('should load user data', () => {
-        startDatabase();
-        return insertAd({title: 'test data'})
-            .then(data => {
-                expect(data).toBeDefined();
-                expect(data.url).toBeDefined()
-            })
+describe('#Test RestApi end points', () => {
+    it('should load test data', async done => {
+        const res = await request.get('/');
+        expect(res.status).toBe(200)
+        done();
+    });
+    it('should get urls', async done => {
+        const res = await request.get('/urls');
+        expect(res.status).toBe(200)
+        done();
+    })
+    it('should get urls', async done => {
+        const res = await request.get('/data');
+        expect(res.status).toBe(200)
+        done();
     })
 });
